ab5b0494658ddc37a173c0df4b24250aea16a407
Rizky Athallah <muhammad.rizky@ui.ac.id>
2021-11-10 16:31:35 +0700

    xx

--- pergipergi/src/main/java/apap/tutorial/pergipergi/repository/TourGuideDb.java
+++ pergipergi/src/main/java/apap/tutorial/pergipergi/repository/TourGuideDb.java
@@ -4,7 +4,6 @@ import apap.tutorial.pergipergi.model.TourGuideModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface TourGuideDb extends JpaRepository<TourGuideModel, Long> {
