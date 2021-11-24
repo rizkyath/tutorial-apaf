26457589b9fa119193ffa17ce533a353e7cde899
Rizky Athallah <muhammad.rizky@ui.ac.id>
2021-11-24 19:37:51 +0700

    tutorial7: Latihan not yet implemented

--- frontend/src/components/List/index.js
+++ frontend/src/components/List/index.js
@@ -6,7 +6,7 @@ export default function List({ title, items, onItemClick, isShopList }) {
         <>
             <h3 style={StyleSheet.heading}>{title}</h3>
             <div className="list-group">
-                {items.length==0 ? (
+                {items.length===0 ? (
                     <div className="text-center">
                         <h1>Belum ada item yang dipilih</h1>
                         <h3>Klik salah satu item di List Item</h3>
--- frontend/src/views/Home/index.js
+++ frontend/src/views/Home/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import listItems from "../../items.json";
 import List from "../../components/List/index";
 import "./index.css";
+import { Fab } from "@material-ui/core";
+import Badge from "@material-ui/core/Badge";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import ViewStreamIcon from "@mui/icons-material/ViewStream";
 
 export default class Home extends React.Component {
     constructor(props) {
@@ -14,27 +18,69 @@ export default class Home extends React.Component {
         };
     }
 
+    // render() {
+    //     return (
+    //         <div className="containter-fluid">
+    //             <h1 className="text-center mt-3 mb-0"> Mini Commerce</h1>
+    //             <p className="text-center text-secondary text-sm font-italic">
+    //                 (this is a <strong>class-based</strong> application)
+    //             </p>
+    //             <div className="container pt-3">
+    //                 <div className="row mt-3">
+    //                     <div className="col-sm">
+    //                         <List
+    //                             title="List Items"
+    //                             items={listItems}
+    //                             onItemClick={() => {}}
+    //                         ></List>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     );
+    // }
+
     render() {
         return (
-            <div className="containter-fluid">
-                <h1 className="text-center mt-3 mb-0"> Mini Commerce</h1>
+            <div className="container-fluid">
+                <h1 className="text-center mt-3 mb-0">Mini Commerce</h1>
+                <div style={{ position: "fixed", top:25, right:25}}>
+                    <Fab variant="extended" onClick={this.handleToggle}>
+                        {this.state.cartHidden ?
+                            <Badge color="secondary" badgeContent={this.state.cartItems.length}>
+                                <ShoppingCartIcon />
+                            </Badge>
+                            : <ViewStreamIcon/>}
+                    </Fab>
+                </div>
                 <p className="text-center text-secondary text-sm font-italic">
                     (this is a <strong>class-based</strong> application)
                 </p>
+                <p className="text-center text-primary"> Your Balance: <b>{this.state.balance}</b></p>
                 <div className="container pt-3">
                     <div className="row mt-3">
-                        <div className="col-sm">
+                        {!this.state.cartHidden ? (
+                            <div className="col-sm">
+                                <List
+                                    title="My Cart"
+                                    items={this.state.cartItems}
+                                    onItemClick={() => {}}
+                                ></List>
+                            </div>
+                        ) : <div className="col-sm">
                             <List
                                 title="List Items"
-                                items={listItems}
-                                onItemClick={() => {}}
+                                items={this.state.shopItems}
+                                onItemClick={this.handleAddItemToCart}
+                                isShopList={true}
                             ></List>
-                        </div>
+                            </div>}
                     </div>
                 </div>
             </div>
         );
-    }
+    };
+
 
     handleAddItemToCart = (item) => {
         const newItems = [...this.state.cartItems];
@@ -49,10 +95,26 @@ export default class Home extends React.Component {
         this.setState({ cartItems: newItems });
     };
 
+    // handleDeleteItemFromCart = (item) => {
+    //     const newItem = {...item};
+    //     const targetInd = newItems.findIndex((it) => it.id === newItem.id);
+
+    //     if (targetInd < 0) {
+            
+    //     }
+    // }
+
     updateShopItem = (item, inCart) => {
         const tempShopItems = this.state.shopItems;
         const targetInd = tempShopItems.findIndex((it) => it.id === item.id);
         tempShopItems[targetInd].inCart = inCart;
         this.setState({ shopItems: tempShopItems });
     };
+
+    handleToggle = () => {
+        const cartHidden = this.state.cartHidden;
+        this.setState({ cartHidden: !cartHidden });
+    };
+
+
 }
