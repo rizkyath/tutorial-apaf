0e5aee81dae887b9bbc9703dc3024b0813dbad53
Rizky Athallah <muhammad.rizky@ui.ac.id>
2022-01-08 00:22:14 +0700

    edit user table name

--- pergipergi/src/main/java/apap/tutorial/pergipergi/model/UserModel.java
+++ pergipergi/src/main/java/apap/tutorial/pergipergi/model/UserModel.java
@@ -19,7 +19,7 @@ import java.io.Serializable;
 @Setter
 @Getter
 @Entity
-@Table(name="user")
+@Table(name="pengguna")
 public class UserModel implements Serializable {
     @Id
     @GeneratedValue(generator = "system-uuid")
